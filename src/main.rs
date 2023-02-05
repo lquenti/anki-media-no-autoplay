7c20d004fe1031d8c0716d5f118620ddcb69fe47
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-02-05 16:16:52 +0100

    Explaination in code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+/*
+ * Path to the actual assets
+ */
 const AUDIO_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/Bruh.mp3");
 const VIDEO_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/aaaa.mp4");
 
@@ -28,6 +31,33 @@ fn main() -> Result<(), Error> {
             .afmt(r#"{{FrontSide}}<hr id="answer">{{Answer}}"#)],
     );
 
+    /*
+     * The second argument is a vector with the following elements:
+     * 1. The text field for the "Question" Field as specified in the model
+     * 2. The text field for the "Answer" Field
+     * 3. The text field for the "Hidden" Field
+     *
+     * Thus, as specified in the model, the front will be
+     * > Question Text
+     * and the back will be
+     * > Question Text
+     * > <hr id="answer">
+     * > Answer Text
+     * > <audio controls>
+     * >   <source src=\"Bruh.mp3\" type=\"audio/mpeg\">
+     * > </audio>
+     *
+     * The audio controls create a browser-supported UI interface instead of Ankis auto-play-only
+     * support provided by [sound:FILENAME.mp3].
+     *
+     * The auto play does not get triggered, as Hidden is neither embedded in the front nor the
+     * back side. So why do we include it at all?
+     *
+     * Anki will ignore any media that is not referenced in at least one note. Thus, we just
+     * reference it so that it will be available in the Anki media so that we can use the
+     * browser-powered methods for accessing it.
+     *
+     */
     let audio_note = Note::new(
         my_model.clone(),
         vec![
@@ -39,6 +69,11 @@ fn main() -> Result<(), Error> {
             "[sound:Bruh.mp3]",
         ],
     )?;
+
+    /*
+     * Same idea but for videos.
+     * Even more important so that we do not have an external video player popping up
+     */
     let video_note = Note::new(
         my_model.clone(),
         vec![
@@ -51,10 +86,13 @@ fn main() -> Result<(), Error> {
         ],
     )?;
 
-    // The rest is boilerplate
+    // Boilerplate
     let mut my_deck = Deck::new(2059400110, "Testdeck Media", "Example Description");
     my_deck.add_note(audio_note);
     my_deck.add_note(video_note);
+
+    // Note that we have to include the initial files so that they can be packaged in the apkg
+    // (which is a zip btw)
     let mut my_package = Package::new(vec![my_deck], vec![AUDIO_PATH, VIDEO_PATH])?;
     my_package.write_to_file("output.apkg")?;
     Ok(())
